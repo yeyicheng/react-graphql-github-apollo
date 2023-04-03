9b4ac7b7b91b0a07c07067b9ef4fe376ed32190b
Yicheng Ye <ye.yicheng123@gmail.com>
2023-04-03 09:35:40 -0700

    v1

--- src/App/Footer/index.js
+++ src/App/Footer/index.js
@@ -8,34 +8,14 @@ const Footer = () => (
   <div className="Footer">
     <div>
       <small>
-        <span className="Footer-text">Built by</span>{' '}
+        <span className="Footer-text">Customized by</span>{' '}
         <Link
           className="Footer-link"
-          href="https://www.robinwieruch.de"
+          href="https://github.com/yichengye310"
         >
-          Robin Wieruch
+          Yicheng Ye
         </Link>{' '}
         <span className="Footer-text">with &hearts;</span>
-      </small>
-    </div>
-    <div>
-      <small>
-        <span className="Footer-text">
-          Interested in GraphQL, Apollo and React?
-        </span>{' '}
-        <Link
-          className="Footer-link"
-          href="https://www.getrevue.co/profile/rwieruch"
-        >
-          Get updates
-        </Link>{' '}
-        <span className="Footer-text">
-          about upcoming articles, books &
-        </span>{' '}
-        <Link className="Footer-link" href="https://roadtoreact.com">
-          courses
-        </Link>
-        <span className="Footer-text">.</span>
       </small>
     </div>
   </div>
